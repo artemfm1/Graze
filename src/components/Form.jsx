57dbba0db5c395e2fbdd66b38d4ac80b4a5fee75
Artem Furman <af@Artems-MBP.fios-router.home>
2021-05-07 16:12:59 -0400

    form updating state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
+
 function Form() {
+  const [location, setLocation] = useState(" ");
+  const [restaurant, setRestaurant] = useState(" ");
+  const [favoriteDish, setFavoriteDish] = useState(" ");
+  const [review, setReview] = useState(" ");
+
   return (
     <form action="">
-      <label htmlFor=""></label><input type="text" />
-      <label htmlFor=""></label><input type="text" />
-      <label htmlFor=""></label><input type="text" />
-      <label htmlFor=""></label><input type="text" /> <button>
 
-      </button>
+      <label htmlFor="location">location</label>
+      <input
+        type="text"
+        id="location"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)} />
+      
+      <label htmlFor="restaurant">restaurant</label>
+      <input
+        type="text"
+        id="restaurant"
+        value={restaurant}
+        onChange={(e) => setRestaurant(e.target.value)} />
+      
+      <label htmlFor="favoriteDish">favorite dish</label>
+      <input
+        type="text"
+        id="favoriteDish"
+        value={favoriteDish}
+        onChange={(e) => setFavoriteDish(e.target.value)} />
+      
+      <label htmlFor="review">review</label>
+      <input
+        type="text"
+        id="review"
+        value={review}
+        onChange={(e) => setReview(e.target.value)} />
+      
+      
+
+      <button type="submit">Post</button>
     </form>
-  )
+  );
 }
-
 
 export default Form;
